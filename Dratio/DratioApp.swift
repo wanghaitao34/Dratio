495c6d8503ef57cc37ee4b3675cb0cf2a3702a03
jingbenzhengyi <wht@jingbenzhengyi.com>
2026-04-03 20:19:58 +0800

    Initial Commit

---
+++ Dratio/DratioApp.swift
@@ -0,0 +1,17 @@
+//
+//  DratioApp.swift
+//  Dratio
+//
+//  Created by Hector on 3/4/26.
+//
+
+import SwiftUI
+
+@main
+struct DratioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
